50b917dc0a1007a05dac9b5617aaea6fb83d2e2d
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-02 12:02:34 -0600

    adds test data and modifies the test descriptions slightly

--- batterup/src/components/Dashboard/Dashboard.js
+++ batterup/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-class componentName extends Component {
+class Dashboard extends Component {
   render() {
     return <div>This is the dashboard</div>;
   }
 }
 
-export default componentName;
+export default Dashboard;
---
+++ batterup/src/testData.js
@@ -0,0 +1,39 @@
+module.exports = {
+  testData: [
+    {
+      player: 'Payer 1',
+      balls: 0,
+      strikes: 0
+    },
+    {
+      player: 'Payer 2',
+      balls: 3,
+      strikes: 0
+    },
+    {
+      player: 'Payer 3',
+      balls: 0,
+      strikes: 2
+    },
+    {
+      player: 'Payer 4',
+      balls: 0,
+      strikes: 0
+    },
+    {
+      player: 'Payer 5',
+      balls: 0,
+      strikes: 0
+    },
+    {
+      player: 'Payer 6',
+      balls: 0,
+      strikes: 0
+    },
+    {
+      player: 'Payer 7',
+      balls: 0,
+      strikes: 0
+    }
+  ]
+};
