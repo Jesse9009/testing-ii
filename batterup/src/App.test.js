86d779d71f4ee5e9930e09e6fdd2bfc42f8613e4
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-02 11:51:52 -0600

    Creates descriptions of the tests that need to be done for the display and dashboard components

--- batterup/src/App.test.js
+++ batterup/src/App.test.js
@@ -6,8 +6,24 @@ import App from './App';
 import Disaply from './components/Display/Display';
 import Dashboard from './components/Dashboard/Dashboard';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('The Batter Up React App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  describe('the display component', () => {
+    it('displays the correct number of strikes when the "Strike" button is clicked', () => {});
+    it('displays the correct number of balls when the "Ball" button is clicked', () => {});
+    it('resets balls and strikes to 0 if player reaches 3 strikes or 4 balls', () => {});
+    it('resets balls and strikes to 0 if player connects any type of hit(The "hit" button is clicked)', () => {});
+  });
+
+  describe('the dashboard component', () => {
+    it('has a button that for a person to click to increase strike count', () => {});
+    it('has a button that for a person to click to increase ball count', () => {});
+    it('has a button that for a person to click to indicate a foul ball', () => {});
+    it('has a button that for a person to click to indicate a hit', () => {});
+  });
 });
